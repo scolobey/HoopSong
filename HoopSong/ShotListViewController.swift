7d411d49000ccee4904b080553181aa47a2e7888
Ryan Goodwin <minedied@gmail.com>
2023-05-29 15:12:53 -0600

    initial table view functional.

--- HoopSong/ShotListViewController.swift
+++ HoopSong/ShotListViewController.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class ShotListViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var shots: [HoopsShot] = []
     
     @IBOutlet var shotTableview: UITableView!
     
@@ -17,19 +21,39 @@ class ShotListViewController: UIViewController, UITableViewDelegate,  UITableVie
         setupTableView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        fetch()
+        shotTableview.reloadData()
+    }
+    
     func setupTableView() {
         shotTableview.delegate = self
         shotTableview.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return shots.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = shotTableview.dequeueReusableCell(withIdentifier: "shotTableviewCell", for: indexPath)
         cell.backgroundColor = UIColor.white
+        
+        let shot = shots[indexPath.row]
+        cell.textLabel?.text = shot.name
+        
         return cell
     }
 
+    func fetch() {
+        let request = NSFetchRequest<HoopsShot>(entityName: "HoopsShot")
+        
+        do {
+            shots = try context.fetch(request)
+        }
+        catch {
+            print("error fetching shots")
+        }
+    }
 }
--- HoopSong/ShotViewController.swift
+++ HoopSong/ShotViewController.swift
@@ -9,20 +9,58 @@ import UIKit
 
 class ShotViewController: UIViewController, UITextFieldDelegate {
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     @IBOutlet var shotTitleField: UITextField!
+    @IBOutlet var submitButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        shotTitleField.becomeFirstResponder()
         shotTitleField.delegate = self
+        setupSubmitButton()
+    }
+    
+    func setupSubmitButton() {
+        submitButton.addTarget(self, action: #selector(self.submitAction(_:)), for: .touchUpInside)
+    }
+    
+    @objc func submitAction(_ sender:UIButton!) {
+        if let shotName = shotTitleField.text {
+            createShot(shotName: shotName)
+        }
         
+        //dismiss everything and return
+        if let navController = self.navigationController {
+            navController.popViewController(animated: true)
+        }
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if let shotName = textField.text {
-            print("theres text")
+            createShot(shotName: shotName)
+        }
+        
+        //dismiss everything and return
+        if let navController = self.navigationController {
+            navController.popViewController(animated: true)
         }
         
         return true
     }
+    
+    func createShot(shotName: String) {
+        let newShot = HoopsShot(context: context)
+        newShot.name = shotName
+        
+        do {
+            try context.save()
+        }
+        catch {
+            print("error saving context")
+        }
+    }
+    
+
 }
 
